78d1f8638c35216d7bc0a709f905f5d04dd6be68
BetterLordWilliam <willjordan05@icloud.com>
2025-03-13 21:00:30 -0700

    WO: Initial commit, testing project structuring

---
+++ COMP3951_Lab6/TestingApplication.cs
@@ -0,0 +1,10 @@
+namespace COMP3951_Lab6
+{
+    public partial class TestingApplication : Form
+    {
+        public TestingApplication()
+        {
+            InitializeComponent();
+        }
+    }
+}
